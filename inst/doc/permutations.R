b1aaf4627817b4952e72eadc51b457588c323234
Gavin L. Simpson <ucfagls@gmail.com>
2022-01-27 10:50:02 +0000

    version 0.9-7

--- inst/doc/permutations.R
+++ inst/doc/permutations.R
@@ -1,47 +1,23 @@
-### R code from vignette source 'permutations.Rnw'
+## ---- setup, echo = FALSE, results = "hide", message = FALSE, cache = FALSE----
+knitr::opts_chunk$set(message = FALSE, warning = FALSE)
+library("permute")
 
-###################################################
-### code chunk number 1: preliminary
-###################################################
-options("prompt" = "R> ", "continue" = "+  ")
-options(useFancyQuotes="UTF-8")
-suppressWarnings(RNGversion("3.5.0"))
-
-
-###################################################
-### code chunk number 2: load_jackal
-###################################################
+## ---- load_jackal-------------------------------------------------------------
 library("permute")
 data(jackal)
 jackal
 
-
-###################################################
-### code chunk number 3: ttest_jackal
-###################################################
+## ---- ttest_jackal------------------------------------------------------------
 jack.t <- t.test(Length ~ Sex, data = jackal, var.equal = TRUE,
                  alternative = "greater")
 jack.t
 
-
-###################################################
-### code chunk number 4: ftest_jackal
-###################################################
-var.test(Length ~ Sex, data = jackal)
-fligner.test(Length ~ Sex, data = jackal)
-
-
-###################################################
-### code chunk number 5: meanFun
-###################################################
+## ---- meanFun-----------------------------------------------------------------
 meanDif <- function(x, grp) {
  mean(x[grp == "Male"]) - mean(x[grp == "Female"])
 }
 
-
-###################################################
-### code chunk number 6: randJackal
-###################################################
+## ---- randJackal--------------------------------------------------------------
 Djackal <- numeric(length = 5000)
 N <- nrow(jackal)
 set.seed(42)
@@ -51,66 +27,39 @@ for(i in seq_len(length(Djackal) - 1)) {
 }
 Djackal[5000] <- with(jackal, meanDif(Length, Sex))
 
+## ---- hist_jackal, fig=FALSE, echo=TRUE, eval=FALSE---------------------------
+#  hist(Djackal, main = "",
+#       xlab = expression("Mean difference (Male - Female) in mm"))
+#  rug(Djackal[5000], col = "red", lwd = 2)
 
-###################################################
-### code chunk number 7: hist_jackal (eval = FALSE)
-###################################################
-## hist(Djackal, main = "",
-##      xlab = expression("Mean difference (Male - Female) in mm"))
-## rug(Djackal[5000], col = "red", lwd = 2)
-
-
-###################################################
-### code chunk number 8: permutations.Rnw:122-123
-###################################################
+## -----------------------------------------------------------------------------
 (Dbig <- sum(Djackal >= Djackal[5000]))
 
-
-###################################################
-### code chunk number 9: permutations.Rnw:126-127
-###################################################
+## -----------------------------------------------------------------------------
 Dbig / length(Djackal)
 
-
-###################################################
-### code chunk number 10: draw_hist_jackal
-###################################################
+## ---- draw_hist_jackal, fig=TRUE, echo=FALSE, fig.cap="Distribution of the difference of mean mandible length in random allocations, ten to each sex."----
 hist(Djackal, main = "",
      xlab = expression("Mean difference (Male - Female) in mm"))
 rug(Djackal[5000], col = "red", lwd = 2)
 
-
-###################################################
-### code chunk number 11: permutations.Rnw:139-140
-###################################################
+## -----------------------------------------------------------------------------
 choose(20, 10)
 
-
-###################################################
-### code chunk number 12: show_args
-###################################################
+## ---- show_args---------------------------------------------------------------
 args(shuffle)
 
-
-###################################################
-### code chunk number 13: show_str
-###################################################
+## ---- show_str----------------------------------------------------------------
 str(how())
 
-
-###################################################
-### code chunk number 14: compare_shuffle_sample
-###################################################
+## ---- compare_shuffle_sample--------------------------------------------------
 set.seed(2)
 (r1 <- shuffle(10))
 set.seed(2)
 (r2 <- sample(1:10, 10, replace = FALSE))
 all.equal(r1, r2)
 
-
-###################################################
-### code chunk number 15: series1
-###################################################
+## ---- series1-----------------------------------------------------------------
 set.seed(4)
 x <- 1:10
 CTRL <- how(within = Within(type = "series"))
@@ -118,10 +67,7 @@ perm <- shuffle(10, control = CTRL)
 perm
 x[perm] ## equivalent
 
-
-###################################################
-### code chunk number 16: grid1
-###################################################
+## ---- grid1-------------------------------------------------------------------
 set.seed(4)
 plt <- gl(3, 9)
 CTRL <- how(within = Within(type = "grid", ncol = 3, nrow = 3),
@@ -129,19 +75,13 @@ CTRL <- how(within = Within(type = "grid", ncol = 3, nrow = 3),
 perm <- shuffle(length(plt), control = CTRL)
 perm
 
-
-###################################################
-### code chunk number 17: vis_grid1
-###################################################
+## ---- vis_grid1, keep.source=TRUE---------------------------------------------
 ## Original
 lapply(split(seq_along(plt), plt), matrix, ncol = 3)
 ## Shuffled
 lapply(split(perm, plt), matrix, ncol = 3)
 
-
-###################################################
-### code chunk number 18: grid_2
-###################################################
+## ---- grid_2, keep.source=TRUE------------------------------------------------
 set.seed(4)
 CTRL <- how(within = Within(type = "grid", ncol = 3, nrow = 3,
                             constant = TRUE),
@@ -149,43 +89,22 @@ CTRL <- how(within = Within(type = "grid", ncol = 3, nrow = 3,
 perm2 <- shuffle(length(plt), control = CTRL)
 lapply(split(perm2, plt), matrix, ncol = 3)
 
-
-###################################################
-### code chunk number 19: series_2
-###################################################
+## ---- series_2, results="hide"------------------------------------------------
 how(nperm = 10, within = Within(type = "series"))
 
-
-###################################################
-### code chunk number 20: shuffleSet_1
-###################################################
+## ---- shuffleSet_1------------------------------------------------------------
 set.seed(4)
 CTRL <- how(within = Within(type = "series"))
 pset <- shuffleSet(10, nset = 5, control = CTRL)
 pset
 
-
-###################################################
-### code chunk number 21: permutations.Rnw:253-254
-###################################################
+## ---- results="hide"----------------------------------------------------------
 how(nperm = 999)
 
-
-###################################################
-### code chunk number 22: withinArgs
-###################################################
+## ---- withinArgs, echo=FALSE--------------------------------------------------
 args(Within)
 
-
-###################################################
-### code chunk number 23: change-continuation
-###################################################
-options("prompt" = " ", "continue" = " ")
-
-
-###################################################
-### code chunk number 24: ptest-fun
-###################################################
+## ---- ptest-fun---------------------------------------------------------------
 pt.test <- function(x, group, nperm = 199) {
     ## mean difference function
     meanDif <- function(i, x, grp) {
@@ -207,30 +126,12 @@ pt.test <- function(x, group, nperm = 199) {
     Ds / (nperm + 1)     # what proportion of perms is this (the pval)?
 }
 
-
-###################################################
-### code chunk number 25: reset-continuation
-###################################################
-options("prompt" = "R> ", "continue" = "+  ")
-
-
-###################################################
-### code chunk number 26: run-ptest
-###################################################
+## ---- run-ptest---------------------------------------------------------------
 set.seed(42) ## same seed as earlier
 pval <- with(jackal, pt.test(Length, Sex, nperm = 4999))
 pval
 
-
-###################################################
-### code chunk number 27: change-continuation
-###################################################
-options("prompt" = " ", "continue" = " ")
-
-
-###################################################
-### code chunk number 28: parallel-ptest-fun
-###################################################
+## ---- parallel-ptest-fun------------------------------------------------------
 ppt.test <- function(x, group, nperm = 199, cores = 2) {
     ## mean difference function
     meanDif <- function(i, .x, .grp) {
@@ -259,91 +160,51 @@ ppt.test <- function(x, group, nperm = 199, cores = 2) {
     Ds / (nperm + 1)     # what proportion of perms is this (the pval)?
 }
 
-
-###################################################
-### code chunk number 29: reset-continuation
-###################################################
-options("prompt" = "R> ", "continue" = "+  ")
-
-
-###################################################
-### code chunk number 30: run-pptest
-###################################################
+## ---- run-pptest--------------------------------------------------------------
 require("parallel")
 set.seed(42)
 system.time(ppval <- ppt.test(jackal$Length, jackal$Sex, nperm = 9999,
                               cores = 2))
 ppval
 
-
-###################################################
-### code chunk number 31: run-pptest2
-###################################################
+## ---- run-pptest2-------------------------------------------------------------
 set.seed(42)
 system.time(ppval2 <- ppt.test(jackal$Length, jackal$Sex, nperm = 9999,
                                cores = 1))
 ppval2
 
-
-###################################################
-### code chunk number 32: get-set-eg0
-###################################################
+## ---- get-set-eg0-------------------------------------------------------------
 hh <- how()
 
-
-###################################################
-### code chunk number 33: get-set-eg1
-###################################################
+## ---- get-set-eg1-------------------------------------------------------------
 getNperm(hh)
 
-
-###################################################
-### code chunk number 34: <get-set-eg2
-###################################################
+## ---- <get-set-eg2------------------------------------------------------------
 getCall(hh)
 setNperm(hh) <- 999
 getNperm(hh)
 getCall(hh)
 
-
-###################################################
-### code chunk number 35: get-set-eg3
-###################################################
+## ---- get-set-eg3-------------------------------------------------------------
 hh <- how(within = Within(type = "series"),
           plots = Plots(type = "series", strata = gl(10, 5)),
           blocks = gl(5, 10))
 
-
-###################################################
-### code chunk number 36: get-set-eg4
-###################################################
+## ---- get-set-eg4-------------------------------------------------------------
 pl <- getPlots(hh)
 setType(pl) <- "free"
 setPlots(hh) <- pl
 
-
-###################################################
-### code chunk number 37: get-set-eg5
-###################################################
+## ---- get-set-eg5-------------------------------------------------------------
 getType(hh, which = "plots")
 
-
-###################################################
-### code chunk number 38: get-set-eg6
-###################################################
+## ---- get-set-eg6-------------------------------------------------------------
 getCall(getPlots(hh))
 
-
-###################################################
-### code chunk number 39: get-set-eg7
-###################################################
+## ---- get-set-eg7-------------------------------------------------------------
 hh <- update(hh, plots = update(getPlots(hh), type = "series"))
 getType(hh, which = "plots")
 
-
-###################################################
-### code chunk number 40: seesionInfo
-###################################################
-toLatex(sessionInfo(), locale = FALSE)
-
+## ---- seesionInfo, echo=FALSE-------------------------------------------------
+sessioninfo::session_info()
 
